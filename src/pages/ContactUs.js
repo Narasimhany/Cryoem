1b489628e6d7d56cc324e12e310cfb339f5f1e82
Narasimhany <61724564+Narasimhany@users.noreply.github.com>
2025-12-10 18:31:12 +0000

    Made changes  across all components

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -30,7 +30,7 @@ const ContactUs = () => {
         <div className="pricing-card">
           <div className="pricing-header free-header">Free 30 Day Trial</div>
           <ul>
-            <li>Full access to the CryoCloud web-application for Cryo-EM data processing</li>
+            <li>Full access to the PractiCloud web-application for Cryo-EM data processing</li>
             <li>No setup or infrastructure needed</li>
             <li>20 hours of compute time</li>
             <li>2 TB of storage</li>
@@ -41,11 +41,11 @@ const ContactUs = () => {
         <div className="pricing-card">
           <div className="pricing-header paid-header">Paid Subscription</div>
           <ul>
-            <li>Full access to the CryoCloud web-application for Cryo-EM data processing</li>
+            <li>Full access to the PractiCloud web-application for Cryo-EM data processing</li>
             <li>No setup or infrastructure required</li>
             <li>Flexible compute hour bundles for research teams</li>
             <li>Add-ons available for extra hours or more users</li>
-            <li>Access to CryoCloud Uploader during data acquisition</li>
+            <li>Access to PractiCloud Uploader during data acquisition</li>
             <li>Pay-per-use storage & archival monthly</li>
             <li>Enterprise options such as custom VPC deployment</li>
           </ul>
